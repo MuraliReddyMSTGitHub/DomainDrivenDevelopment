6639a64f712db6f0eaf4958a03e24735a3d6a8a9
MURALI MOHAN REDDY P <muralireddymst@gmail.com>
2024-07-16 06:09:38 +0530

    Add files via upload

---
+++ Host.WebApi/ApplicationBuilderExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Common.Web.Middleware;
+using Microsoft.AspNetCore.Builder;
+
+namespace Host.WebApi
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder AddGlobalErrorHandler(this IApplicationBuilder applicationBuilder) => applicationBuilder.UseMiddleware<GlobalExceptionHandler>();
+    }
+}
---
+++ Host.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+﻿using DDD.Domain.Dtos;
+using DDD.Infrastructure.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Host.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this._productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetItems()
+        {
+            try
+            {
+                var products = await this._productRepository.GetItems();
+                var productCategoreis = await this._productRepository.GetProductCategories();
+
+                if (products == null || productCategoreis == null)
+                    return NotFound();
+                else
+                    return Ok(products.ConvertToDto(productCategoreis));
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, " Error retrieving data from the database.");
+            }
+        }
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ProductDto>> GetProduct(int id)
+        {
+            try
+            {
+                var product = await this._productRepository.GetProduct(id);
+                var productCategory = await this._productRepository.GetProductCategory(product.CategoryId);
+
+                if (product == null)
+                    return BadRequest();
+                else
+                    return Ok(product.ConvertToDto(productCategory));
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, " Error retrieving data from the database.");
+            }
+        }
+    }
+}
